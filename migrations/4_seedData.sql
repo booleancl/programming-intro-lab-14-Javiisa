3774eb60730b61b7cd0fd7a0eab389c163ad575e
charlotte <charlottediaz88@gmail.com>
2022-10-17 11:12:07 -0300

    blog

--- migrations/4_seedData.sql
+++ migrations/4_seedData.sql
@@ -1,1 +1,7 @@
+\c blog
+
+\COPY users(email) FROM 'C:/Users/user/intro_dev/programming-intro-lab-14-Javiisa/data/users.csv' csv HEADER;
+\COPY posts FROM 'C:/Users/user/intro_dev/programming-intro-lab-14-Javiisa/data/posts.csv' csv HEADER;
+\COPY comments FROM 'C:/Users/user/intro_dev/programming-intro-lab-14-Javiisa/data/comentarios.csv'csv HEADER;
+
 
